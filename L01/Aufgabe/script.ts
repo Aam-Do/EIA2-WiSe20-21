4aaca1dfa5e0378b33626d29f3612f9677c67cf9
Am.D <ameliedelloro@gmail.com>
2021-10-13 21:41:18 +0200

    lint fix

--- L01/Aufgabe/script.ts
+++ L01/Aufgabe/script.ts
@@ -5,19 +5,19 @@ namespace RandomPoem {
     let objects: string[] = ["the Triforce", "a cucco", "Saria", "the Master Sword", "rupees", "fire Keese"]
     
 
-    for (let i = subjects.length; i > 0; i--) {
+    for (let i: number = subjects.length; i > 0; i--) {
         let y: string = getVerse(subjects, verbs, objects);
         console.log(y);
     }
 
-    function getVerse(_subjects:string[], _verbs:string[], _objects:string[]): string {
+    function getVerse(_subjects: string[], _verbs: string[], _objects: string[]): string {
 
         let verse: string = "";
-        let randomSubject:number = Math.floor(Math.random()*_subjects.length);
+        let randomSubject: number = Math.floor(Math.random()*_subjects.length);
         verse += _subjects.splice(randomSubject, 1)[0] + " ";
-        let randomVerb:number = Math.floor(Math.random()*_verbs.length);
+        let randomVerb: number = Math.floor(Math.random()*_verbs.length);
         verse += _verbs.splice(randomVerb, 1)[0] + " ";
-        let randomObject:number = Math.floor(Math.random()*_objects.length);
+        let randomObject: number = Math.floor(Math.random()*_objects.length);
         verse += _objects.splice(randomObject, 1)[0];
 
         return(verse);
