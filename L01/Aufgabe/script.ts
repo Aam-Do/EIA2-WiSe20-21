1122769b0741fb84b5e0764d8b2ccbcde05f8416
Am.D <ameliedelloro@gmail.com>
2021-10-13 21:43:28 +0200

    lint fix

--- L01/Aufgabe/script.ts
+++ L01/Aufgabe/script.ts
@@ -1,8 +1,8 @@
 namespace RandomPoem {
 
-    let subjects: string[] = ["Zelda", "Link", "Ganondorf", "Ruto", "Sheik", "Impa"]
-    let verbs: string[] = ["loves", "hates", "destroys", "picks up", "is scared of", "finds"]
-    let objects: string[] = ["the Triforce", "a cucco", "Saria", "the Master Sword", "rupees", "fire Keese"]
+    let subjects: string[] = ["Zelda", "Link", "Ganondorf", "Ruto", "Sheik", "Impa"];
+    let verbs: string[] = ["loves", "hates", "destroys", "picks up", "is scared of", "finds"];
+    let objects: string[] = ["the Triforce", "a cucco", "Saria", "the Master Sword", "rupees", "fire Keese"];
     
 
     for (let i: number = subjects.length; i > 0; i--) {
@@ -13,11 +13,11 @@ namespace RandomPoem {
     function getVerse(_subjects: string[], _verbs: string[], _objects: string[]): string {
 
         let verse: string = "";
-        let randomSubject: number = Math.floor(Math.random()*_subjects.length);
+        let randomSubject: number = Math.floor(Math.random() * _subjects.length);
         verse += _subjects.splice(randomSubject, 1)[0] + " ";
-        let randomVerb: number = Math.floor(Math.random()*_verbs.length);
+        let randomVerb: number = Math.floor(Math.random() * _verbs.length);
         verse += _verbs.splice(randomVerb, 1)[0] + " ";
-        let randomObject: number = Math.floor(Math.random()*_objects.length);
+        let randomObject: number = Math.floor(Math.random() * _objects.length);
         verse += _objects.splice(randomObject, 1)[0];
 
         return(verse);
