19558b0b8f7175f8987f9cda82c51d1c9979a4e5
Aam_Do <ameliedelloro@gmail.com>
2021-11-18 13:07:18 +0100

    beauty

--- L08/L08.1/script.ts
+++ L08/L08.1/script.ts
@@ -9,13 +9,70 @@ function handleLoad(_event: Event): void {
 }
 
 function bePog(): void {
-    crc2.fillStyle = "#FF0000";
+
+    let gradient1: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+
+    let hue: string[] = [];
+
+    for (let i = 0; i < 3; i++) {
+        let random: number = Math.floor(360 * Math.random());
+        hue.push(random.toString());  
+    }
+
+    let hsl: string = "hsl(";
+    let saturation: string = ", 77%";
+    let light: string = ", 67%)"
+
+    gradient1.addColorStop(0, hsl + hue[0] + saturation + light);
+    gradient1.addColorStop(.5, hsl + hue[1] + saturation + light);
+    gradient1.addColorStop(1, hsl + hue[2] + saturation + light);
+
+    crc2.fillStyle = gradient1;    
     crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     
-    crc2.strokeStyle = "#0000FF";
+    let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+
+    for (let i = 0; i < 3; i++) {
+        let random: number = Math.floor(360 * Math.random());
+        hue.push(random.toString());  
+    }
+
+
+    gradient.addColorStop(0, hsl + hue[3] + saturation + light);
+    gradient.addColorStop(.5, hsl + hue[4] + saturation + light);
+    gradient.addColorStop(1, hsl + hue[5] + saturation + light);
+
+    crc2.rotate(-30 * Math.PI / 180);
+    crc2.scale(2, 1.2);
+
+    crc2.fillStyle = gradient;    
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     
-    crc2.fillStyle = "#FFFF00";
-    crc2.fillText("helo", 50, 50);
+    crc2.resetTransform();
 
+    let random1: number = Math.floor(14 * Math.random()) + 1;
+    
+    for (let i = 0; i < random1; i++) {
 
+        crc2.strokeStyle = "#0000FF";
+        crc2.fillStyle = "#FF0000";
+        crc2.beginPath();
+
+        crc2.translate((crc2.canvas.width * Math.random()), (crc2.canvas.height * Math.random()));
+        crc2.rotate((320 * Math.random()) * Math.PI / 180);
+        crc2.scale(((2 * Math.random()) + 1), (2 * Math.random()));
+        
+        crc2.moveTo(-25, -25);
+        
+        crc2.lineTo(-25, 25);
+        crc2.lineTo(30, 0);
+
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+        
+        crc2.resetTransform();
+    }
+
+    
 }
