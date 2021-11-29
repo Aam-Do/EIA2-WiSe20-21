d329f3dc783519a4f492f94c31872a6e48b7678a
Aam_Do <ameliedelloro@gmail.com>
2021-11-29 16:00:49 +0100

    Vektoren test

---
+++ L09/L09.1/test.js
@@ -0,0 +1,28 @@
+"use strict";
+var Test;
+(function (Test) {
+    debugger;
+    class Vector {
+        x = 0;
+        y = 0;
+        constructor(_x, _y) {
+            this.set(_x, _y);
+        }
+        set(_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        }
+        scale(_factor) {
+            this.x *= _factor;
+            this.y *= _factor;
+        }
+        add(_addend) {
+            this.x += _addend.x;
+            this.y += _addend.y;
+        }
+    }
+    let v1 = new Vector(2, 3);
+    v1.scale(2);
+    console.log(v1);
+})(Test || (Test = {}));
+//# sourceMappingURL=test.js.map
